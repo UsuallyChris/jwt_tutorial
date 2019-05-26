34e7cdebf4674f3db2c89310eee3398d4ba22249
UsuallyChris <schoonc@gmail.com>
2019-05-26 15:43:15 -0700

    Created project structure, set DB values in .env file, installed dotenv as dependency

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 
 const app = express();
+dotenv.config();
+
+// CONNECT TO DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+  () => {console.log("Connected to db.")}
+);
 
 // IMPORT ROUTES
 const authRoute = require('./routes/auth');
