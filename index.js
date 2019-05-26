4e15d1d7bfc3903bd33d40166fb4aa5e1f568e3c
UsuallyChris <schoonc@gmail.com>
2019-05-26 14:40:46 -0700

    Created start script using nodemon

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// IMPORT ROUTES
+const authRoute = require('./routes/auth');
+
+// ROUTE MIDDLEWARE
+app.use('/api/user', authRoute);
+
 app.listen(3000, () => {
   console.log("Running");
 })
