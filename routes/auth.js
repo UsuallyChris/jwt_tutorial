bfc499c8eaf35fbe10ada897c55b0e374c8d7e99
UsuallyChris <schoonc@gmail.com>
2019-05-27 14:05:16 -0700

    Basic validation implemented

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,24 @@ const router = require('express').Router();
 
 const User = require('../models/User');
 
+
+// VALIDATION
+const Joi = require('@hapi/joi');
+
+const schema = {
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required()
+}
+
 router.post('/register', async (req, res) => {
+
+  // validate user
+  const { error } = Joi.validate(req.body, schema);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
